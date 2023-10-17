b1fe34f5f21aa95b9e704fbdf2f3cf4487e79c9e
Ruben Andre Lima Fernandes <rlima-fe@c1r9s11.42porto.com>
2023-10-17 17:17:38 +0100

    uwu

--- game/errors.c
+++ game/errors.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_check.c                                      :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlima-fe <rlima-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:43:32 by rlima-fe          #+#    #+#             */
-/*   Updated: 2023/10/17 13:43:46 by rlima-fe         ###   ########.fr       */
+/*   Updated: 2023/10/17 15:17:53 by rlima-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,15 @@ void	check_errors(t_comp *game)
 {
 	check_walls(game);
 	char_checker(game);
+}
+
+void	exit_game(t_comp *game)
+{
+	int	line;
+
+	line = 0;
+	while (line < game->height - 1)
+		free(game->map[line++]);
+	free (game->map);
+	exit (0);
 }
